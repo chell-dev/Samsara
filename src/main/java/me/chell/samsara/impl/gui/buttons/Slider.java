f72529a6e1db6bf857a4fc86287b62468e4dc68f
chell <chell@github.com>
2021-11-27 14:47:30 +0100

    keyboard input for sliders

--- src/main/java/me/chell/samsara/impl/gui/buttons/Slider.java
+++ src/main/java/me/chell/samsara/impl/gui/buttons/Slider.java
@@ -1,18 +1,31 @@
 package me.chell.samsara.impl.gui.buttons;
 
+import com.google.common.collect.Lists;
 import me.chell.samsara.api.gui.GuiTheme;
+import me.chell.samsara.api.util.Wrapper;
 import me.chell.samsara.api.value.Value;
+import me.chell.samsara.impl.gui.ModuleButton;
 import me.chell.samsara.impl.gui.ValueButton;
 import net.minecraft.util.math.MathHelper;
+import org.apache.commons.lang3.StringUtils;
+import org.lwjgl.input.Keyboard;
 
-public class Slider<T> extends ValueButton<T> {
+import java.awt.*;
+import java.util.List;
+
+public class Slider<T extends Number> extends ValueButton<T> {
     private int sliderWidth = 0;
     private boolean grabbed = false;
     private Type type;
 
+    private int ticks;
+    private boolean editing = false;
+    private String oldText, editText;
+
     //@SuppressWarnings("unchecked")
     public Slider(Value<T> value, int x, int y) {
         super(x, y, GuiTheme.width, 13, value);
+        editText = ""+value.getValue();
 
         // TODO fix
         if(value.getValue() instanceof Integer){
@@ -83,8 +96,18 @@ public class Slider<T> extends ValueButton<T> {
         drawThemedRectTertiary(x, y, 2, height);
         drawThemedRectTertiary(x+2, y+height-1, width-2, 1);
 
+        String displayText;
+        if(editing) {
+            displayText = editText;
+            if(ticks / 6 % 2 == 0) displayText += "_";
+        } else {
+            displayText = ""+value.getValue();
+        }
+
         drawThemedString(value.getDisplayName(), x + 4, getStringCenterY(y, height-1));
-        drawThemedStringRight(""+value.getValue(), x + width - 2, getStringCenterY(y, height-1));
+        int rightX = x + width - 2;
+        if(displayText.endsWith("_")) rightX += Wrapper.getFontRenderer().getCharWidth('_');
+        drawThemedStringRight(displayText, rightX, getStringCenterY(y, height-1));
     }
 
     @Override
@@ -92,6 +115,13 @@ public class Slider<T> extends ValueButton<T> {
         if(mouseX >= x+2 && mouseX <= x + width && mouseY >= y && mouseY <= y + height-1) {
             if(mouseButton == 0) {
                 grabbed = true;
+                return true;
+            } else if(mouseButton == 2) {
+                if(!editing) {
+                    ticks = 0;
+                    editing = true;
+                    editText = oldText = ""+value.getValue();
+                }
                 return true;
             }
         }
@@ -105,9 +135,71 @@ public class Slider<T> extends ValueButton<T> {
         }
     }
 
+    private final List<Character> numbers = Lists.newArrayList('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-');
+
+    @Override
+    public boolean keyTyped(char typedChar, int keyCode) {
+        if(editing) {
+            switch (keyCode) {
+                case Keyboard.KEY_BACK:
+                    editText = StringUtils.chop(editText);
+                    return true;
+                case Keyboard.KEY_RETURN:
+                    parseValue(editText);
+                    editing = false;
+                    return true;
+                case Keyboard.KEY_ESCAPE:
+                    editing = false;
+                    editText = oldText;
+                    return true;
+                case Keyboard.KEY_V:
+                    if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
+                        editText += Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
+                    } else {
+                        editText += typedChar;
+                    }
+                    return true;
+                default:
+                    if(numbers.contains(typedChar)) {
+                        editText += typedChar;
+                        return true;
+                    }
+            }
+        }
+        return super.keyTyped(typedChar, keyCode);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void parseValue(String text) {
+        switch (type) {
+            case INT:
+                try {
+                    value.setValue((T)(Integer)Integer.parseInt(text));
+                } catch (NumberFormatException ignored){}
+                break;
+            case FLOAT:
+                try {
+                    value.setValue((T)(Float)Float.parseFloat(text));
+                } catch (NumberFormatException ignored){}
+                break;
+            case DOUBLE:
+                try {
+                    value.setValue((T)(Double)Double.parseDouble(text));
+                } catch (NumberFormatException ignored){}
+                break;
+        }
+    }
+
     @Override
     public void onGuiClosed() {
         grabbed = false;
+        editing = false;
+        editText = oldText;
+    }
+
+    @Override
+    public void updateScreen() {
+        ticks++;
     }
 
     private enum Type {
