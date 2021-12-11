064d12895c6adb925809ce708c04e6650f9822d6
chell <no>
2021-12-11 17:15:14 +0000

    start from scratch

--- src/main/java/me/chell/samsara/api/Loadable.java
+++
@@ -1,6 +0,0 @@
-package me.chell.samsara.api;
-
-public interface Loadable {
-    void load();
-    void unload();
-}
--- src/main/java/me/chell/samsara/api/event/PlayerUpdateEvent.java
+++
@@ -1,6 +0,0 @@
-package me.chell.samsara.api.event;
-
-import net.minecraftforge.fml.common.eventhandler.Event;
-
-public class PlayerUpdateEvent extends Event {
-}
