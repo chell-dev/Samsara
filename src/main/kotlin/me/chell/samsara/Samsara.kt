0e3d95123a8e24c8e6ca9fc0bccd5e36731abf68
chell <no>
2021-12-11 18:54:00 +0000

    Loadable and Event system

--- src/main/kotlin/me/chell/samsara/Samsara.kt
+++ src/main/kotlin/me/chell/samsara/Samsara.kt
@@ -1,5 +1,7 @@
 package me.chell.samsara
 
+import me.chell.samsara.api.Loadable
+import me.chell.samsara.api.event.EventManager
 import org.apache.logging.log4j.LogManager
 
 class Samsara {
@@ -9,16 +11,31 @@ class Samsara {
         const val VERSION = "1.0.0"
         const val MODID = "samsara"
         val LOGGER = LogManager.getLogger()
+        lateinit var instance: Samsara
     }
 
+    init {
+        instance = this
+    }
+
+    private val loadables = mutableListOf<Loadable>()
+
+    lateinit var eventManager: EventManager
+    var loaded = false
+
     fun init() {
+        loadables.add(EventManager().also { eventManager = it })
         load()
     }
 
     fun load() {
+        for(l in loadables) l.load()
+        loaded = true
         LOGGER.info("$NAME $VERSION loaded.")
     }
 
     fun unload() {
+        loaded = false
+        for(l in loadables) l.unload()
     }
 }
---
+++ src/main/kotlin/me/chell/samsara/api/event/Event.kt
@@ -0,0 +1,3 @@
+package me.chell.samsara.api.event
+
+open class Event
