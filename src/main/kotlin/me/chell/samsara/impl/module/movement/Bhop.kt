dc364b509db746e6f7f96780c2241fe61e1d59f1
chell <no>
2022-01-21 18:42:17 +0100

    bhop fix

--- src/main/kotlin/me/chell/samsara/impl/module/movement/Bhop.kt
+++ src/main/kotlin/me/chell/samsara/impl/module/movement/Bhop.kt
@@ -3,7 +3,6 @@ package me.chell.samsara.impl.module.movement
 import me.chell.samsara.api.event.EventHandler
 import me.chell.samsara.api.module.Module
 import me.chell.samsara.impl.event.PlayerTickEvent
-import net.minecraft.tag.FluidTags
 
 class Bhop: Module("Bhop", Category.MOVEMENT, "So you don't have to hold space") {
 
@@ -15,9 +14,6 @@ class Bhop: Module("Bhop", Category.MOVEMENT, "So you don't have to hold space")
 
     private fun canJump(): Boolean {
         if(player.input.jumping) return false
-        if(player.isOnGround) return true
-
-        val a = if(player.isInLava) player.getFluidHeight(FluidTags.LAVA) else player.getFluidHeight(FluidTags.WATER)
-        return (player.isTouchingWater && a > 0.0) && a > player.swimHeight
+        return player.isOnGround && !player.isTouchingWater && !player.isInLava
     }
 }
