68b929796cc57493c0bbb547a2f50d852b293a73
chell <no>
2022-02-05 10:54:21 +0100

    remove unused mixins and events

--- src/main/kotlin/me/chell/samsara/impl/event/EntityKnockbackEvent.kt
+++
@@ -1,6 +0,0 @@
-package me.chell.samsara.impl.event
-
-import me.chell.samsara.api.event.Event
-import net.minecraft.entity.Entity
-
-class EntityKnockbackEvent(val entity: Entity): Event.Cancelable()
